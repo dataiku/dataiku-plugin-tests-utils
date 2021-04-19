564fb314fa756adc31f42bebdc5d4c3fb445add6
HenriChabert <henri@famille-chabert.com>
2021-04-19 20:34:27 +0200

    Renaming and logging

--- dku_plugin_test_utils/signal_slack/signal_slack.py
+++ dku_plugin_test_utils/signal_slack/signal_slack.py
@@ -8,7 +8,9 @@ import sys
 
 import requests
 
-BATCH_SIZE = 48
+SLACK_MAX_BLOCKS = 50
+N_BLOCKS_PER_ITEM = 3
+BATCH_SIZE = (SLACK_MAX_BLOCKS // N_BLOCKS_PER_ITEM) * N_BLOCKS_PER_ITEM  # Prevent split blocks that belong together
 
 
 def send_slack_signal(path_to_raw_daily, slack_endpoint):
@@ -136,9 +138,12 @@ def send_slack_signal(path_to_raw_daily, slack_endpoint):
     print("---------")
 
     if blocks:
-        i = 0
-        while i < len(blocks):
-            blocks_batch = blocks[i:i+BATCH_SIZE]
+        batch_n = 0
+        print("Nb batches ----")
+        print("There will be {} batches sent.".format(len(blocks) // BATCH_SIZE))
+        print("------------")
+        while batch_n < len(blocks):
+            blocks_batch = blocks[batch_n:batch_n+BATCH_SIZE]
             batch_payload = cp.deepcopy(payload_base)
             batch_payload["blocks"].extend(blocks_batch)
 
@@ -148,7 +153,7 @@ def send_slack_signal(path_to_raw_daily, slack_endpoint):
 
             x = requests.post(slack_endpoint, data=json.dumps(batch_payload))
             x.raise_for_status()
-            i += BATCH_SIZE
+            batch_n += BATCH_SIZE
 
         for status_file in all_status_files:
             os.remove(os.path.join(path_to_raw_daily, status_file))
